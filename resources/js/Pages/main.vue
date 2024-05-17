62cdd604ef42c603c1e08aea79d74bcd8ba1ddc1
Baxodir <yusufjonov707@gmail.com>
2024-05-17 16:53:32 +0500

    front is start

--- resources/js/Pages/main.vue
+++ resources/js/Pages/main.vue
@@ -1,5 +1,90 @@
-<script setup>
-</script>
 <template>
-    <H1>Hello world</H1>
+    <div class="home">
+        <section class="hero">
+            <div class="hero-text container">
+                <div class="hero-content">
+                    <h4>Vue Responsive Nav menu</h4>
+                    <hr />
+                    <h2>
+                        With Vue <br />
+                        Anmimations
+                    </h2>
+                    <hr />
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
+
+<script>
+export default {
+    name: "Main",
+    components: {},
+};
+</script>
+
+<style scoped>
+.hero {
+    background-image: url("/images/hero-bg.jpeg");
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover; /* This will ensure the image covers the entire element */
+    background-position: center;
+    position: relative;
+    height: 100vh;
+
+    img {
+        object-fit: cover;
+        height: 100%;
+        width: 100%;
+    }
+
+    .hero-text {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        color: #fff;
+
+        h4 {
+            text-transform: uppercase;
+            font-size: 22px;
+            padding-bottom: 4px;
+        }
+
+        h2 {
+            font-size: 50px;
+            @media (min-width: 550px) {
+                font-size: 80px;
+            }
+        }
+
+        hr:nth-child(2) {
+            max-width: 365px;
+            margin-bottom: 16px;
+        }
+        hr:nth-child(4) {
+            height: 6px;
+            background-color: #fff;
+            border: none;
+            max-width: 85px;
+            margin-top: 16px;
+        }
+    }
+}
+.hero::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5); /* Black color with 50% opacity */
+    z-index: 1;
+}
+
+.hero-content {
+    position: relative;
+    z-index: 2; /* Ensures the content appears above the overlay */
+}
+</style>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,4 +9,29 @@
   <body>
     @inertia
   </body>
+  <script>
+    export default {
+        name: "App",
+    }
+  </script>
+  <style>
+    @import url("http://fonts.googleapis.com/css?family=Raleway:wght@400;500&display=swap");
+    * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: "Raleway", sans-serif;
+    font-weight: 400;
+    }
+    .app {
+    min-height: 100vh;
+    position: relative;
+    background-color: #f1f1f1;
+    }
+    .container {
+    padding: 0 20px;
+    max-width: 1140px;
+    margin: 0 auto;
+    }
+  </style>
 </html>
